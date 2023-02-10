995653b94c4425d5253d83e13ee84cb0221b6a75
olfaselmi <47832011+olfaselmi@users.noreply.github.com>
2023-02-10 23:21:05 +0100

    register route

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const connectDB = require("./mongodb/db");
+
 const startServer = require("./utils/startServer");
 
 const auth = require("./routes/api/auth");
+const user = require("./routes/api/user");
 
+
+//Connect Database
+connectDB();
 // server creation
 const app = express();
-
+//Init middleware (Body Parser , now it s included with express )
+app.use(express.urlencoded())
+app.use(express.json()) 
 // API endpoints
 app.use("/api/auth", auth);
+app.use("/api/user", user);
+
 
 // function that starts server
 startServer(app);
+              
---
+++ routes/api/user.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const gravatar = require("gravatar");
+const { check, validationResult } = require("express-validator");
+const User = require("../../mongodb/models/user");
+const bcrypt = require("bcryptjs");
+const router = express.Router();
+
+//@author Olfa selmi
+//@Route POST api/user/register
+//@Description  This is a registeration route
+//@Access Public
+
+router.post(
+  "/register",
+//   [
+//     check("name", "Name is required").not().isEmpty(),
+//     check("email", "Please enter a valid name").isEmail(),
+//     check(
+//       "password",
+//       "Please enter a password with at least 6 characters"
+//     ).isLength({
+//       min: 6,
+//     }),
+//   ],
+  async (req, res) => {
+    // Check inputs validation
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //   return res.status(400).json({ errors: errors.array() });
+    // }
+    const { name, email, password } = req.body;
+    try {
+        console.log("this is the email address ", req.body);
+
+        res.json({"email" : email});
+      // Get user avatar
+    //   const avatar = gravatar.url(email, {
+    //     s: "200",
+    //     r: "pg",
+    //     d: "mm",
+    //   });
+    // This doesn't create the user it just create an inctance of it (we have to implement the .save();)
+    //   user = new User({
+    //     name,
+    //     email,
+    //     avatar,
+    //     password,
+    //   });
+    //   console.log(name, email, avatar, password);
+
+      // Password encryption
+    //   const salt = await bcrypt.genSalt(10);
+    //   // I added the toString() otherwise it didn't work thanks to : https://github.com/bradtraversy/nodeauthapp/issues/7
+    //   user.password = await bcrypt.hash(password.toString(), salt);
+    //   await user.save();
+    //   res.status(200).json({
+    //     msg: "User registered successfully",
+    //     user: user,
+    //   });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
