c7d50fbedb73e605686201b9313d3c53d5883167
olfaselmi <47832011+olfaselmi@users.noreply.github.com>
2023-02-11 23:45:47 +0100

    login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,17 +1,39 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
+const User = require("../../mongodb/models/user");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 //@author Olfa selmi
 //@Route GET api/auth
 //@Description  This is a test route
 //@Access Public
-router.get("/olfa", async (req, res) => {
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
   try {
-    res.send("Hello from auth route");
+    let user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found!" });
+    }
+    let isMatch = bcrypt.compare(password.toString(), user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Wrong password!" });
+    }
+    const payload = { user: { id: user.id, email: user.email } };
+    let jwToken = jwt.sign(payload, "this a scret key");
+    return res.status(200).json({
+      message: "User connected!",
+      user: {
+        _id: user.user,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+      },
+      token: jwToken,
+    });
   } catch (error) {
     console.error(error.message);
-    res.status(500).send("Server error");
+    return res.status(500).send("Server error");
   }
 });
 
--- routes/api/user.js
+++ routes/api/user.js
@@ -4,6 +4,7 @@ const { check, validationResult } = require("express-validator");
 const User = require("../../mongodb/models/user");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 //@author Olfa selmi
 //@Route POST api/user/register
@@ -12,51 +13,52 @@ const router = express.Router();
 
 router.post(
   "/register",
-//   [
-//     check("name", "Name is required").not().isEmpty(),
-//     check("email", "Please enter a valid name").isEmail(),
-//     check(
-//       "password",
-//       "Please enter a password with at least 6 characters"
-//     ).isLength({
-//       min: 6,
-//     }),
-//   ],
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with at least 6 characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
   async (req, res) => {
-    // Check inputs validation
-    // const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //   return res.status(400).json({ errors: errors.array() });
-    // }
+   // Check inputs validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     const { name, email, password } = req.body;
+    
     try {
-        console.log("this is the email address ", req.body);
-
-        res.json({"email" : email});
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'Email already exists' });
+      }
       // Get user avatar
-    //   const avatar = gravatar.url(email, {
-    //     s: "200",
-    //     r: "pg",
-    //     d: "mm",
-    //   });
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
     // This doesn't create the user it just create an inctance of it (we have to implement the .save();)
-    //   user = new User({
-    //     name,
-    //     email,
-    //     avatar,
-    //     password,
-    //   });
-    //   console.log(name, email, avatar, password);
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
 
       // Password encryption
-    //   const salt = await bcrypt.genSalt(10);
-    //   // I added the toString() otherwise it didn't work thanks to : https://github.com/bradtraversy/nodeauthapp/issues/7
-    //   user.password = await bcrypt.hash(password.toString(), salt);
-    //   await user.save();
-    //   res.status(200).json({
-    //     msg: "User registered successfully",
-    //     user: user,
-    //   });
+      const salt = await bcrypt.genSalt(10);
+      // I added the toString() otherwise it didn't work thanks to : https://github.com/bradtraversy/nodeauthapp/issues/7
+      user.password = await bcrypt.hash(password.toString(), salt);
+      await user.save();
+      res.status(200).json({
+        msg: "User registered successfully",
+        user: user,
+      });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server error");
